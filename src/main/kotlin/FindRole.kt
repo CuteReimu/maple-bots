c4a76fe3787c74749ffac52ae0d1fc98a3131a94
CuteReimu <415551921@qq.com>
2024-01-26 18:24:50 +0800

    增加预计升级天数

--- src/main/kotlin/FindRole.kt
+++ src/main/kotlin/FindRole.kt
@@ -9,10 +9,10 @@ import kotlinx.serialization.json.JsonElement
 import kotlinx.serialization.json.jsonObject
 import kotlinx.serialization.serializer
 import net.mamoe.mirai.contact.Group
+import net.mamoe.mirai.message.data.Image
 import net.mamoe.mirai.message.data.Message
 import net.mamoe.mirai.message.data.PlainText
 import net.mamoe.mirai.message.data.toMessageChain
-import net.mamoe.mirai.message.data.toPlainText
 import net.mamoe.mirai.utils.ExternalResource.Companion.toExternalResource
 import net.mamoe.mirai.utils.MiraiLogger
 import okhttp3.Request
@@ -23,6 +23,7 @@ import java.awt.Rectangle
 import java.awt.image.BufferedImage
 import java.io.ByteArrayOutputStream
 import javax.imageio.ImageIO
+import kotlin.math.ceil
 
 object FindRole {
     private val json = Json {
@@ -55,31 +56,28 @@ object FindRole {
         }
         val body = resp.body!!.string()
         val data = json.parseToJsonElement(body).jsonObject["CharacterData"]!!.decode<CharacterData>()
-        val ret = ArrayList<Message>()
 
+        var playerImage: Image? = null
         data.image?.let { image ->
             try {
-                PluginMain.getPic(image).use { `is` ->
+                playerImage = PluginMain.getPic(image).use { `is` ->
                     `is`.toExternalResource().use { group.uploadImage(it) }
-                }.let { ret.add(it) }
+                }
             } catch (e: Exception) {
                 logger.error("获取或上传图片失败", e)
             }
         }
 
-        ret.add(
-            """
+        var s = """
             |角色名：${data.name}
             |职业：${data.`class`}
             |等级：${data.level}(${data.expPercent}%)
             |联盟：${data.legionLevel}
             |""".trimMargin()
-                .let { s -> if (data.graphData != null && data.graphData.any { it.expDifference != 0L }) s else s + "近日无经验变化" }
-                .toPlainText()
-        )
 
+        var expImage: Image? = null
+        val values = ArrayList<Pair<Long, String>>() // expDifference, dateLabel
         if (data.graphData != null) {
-            val values = ArrayList<Pair<Long, String>>()
             for (i in data.graphData.indices) {
                 val levelExp = data.graphData[i].currentExp + data.graphData[i].expToNextLevel
                 if (LevelExpData.data[data.graphData[i].level] != levelExp)
@@ -122,16 +120,28 @@ object FindRole {
                     withContext(Dispatchers.IO) {
                         ImageIO.write(img, "png", buf)
                     }
-                    buf.toByteArray().toExternalResource().use {
+                    expImage = buf.toByteArray().toExternalResource().use {
                         group.uploadImage(it)
-                    }.let { ret.add(it) }
+                    }
                 } catch (e: Exception) {
                     logger.error("上传图片失败", e)
                 }
             }
         }
 
-        return ret.toMessageChain()
+        if (expImage == null) {
+            s += "近日无经验变化"
+        } else if (values.isNotEmpty()) {
+            val sumExp = values.sumOf { it.first }
+            if (sumExp > 0) {
+                LevelExpData.data[data.level]?.let {
+                    val days = ceil((it - data.exp) / (sumExp.toDouble() / values.size)).toInt()
+                    s += "预计还有${days}天升级\n"
+                }
+            }
+        }
+
+        return listOfNotNull(playerImage, PlainText(s), expImage).toMessageChain()
     }
 
     private val logger: MiraiLogger by lazy {
@@ -146,6 +156,9 @@ class CharacterData(
 
     @SerialName("EXPPercent")
     val expPercent: Double = 0.0,
+
+    @SerialName("EXP")
+    val exp: Long = 0,
 
     @SerialName("Level")
     val level: Int = 0,
